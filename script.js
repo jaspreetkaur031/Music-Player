bf3b5e1c64c03bce90c1b89befbe6f625bbd2b15
Bharat <bbhushan50_be23@thapar.edu>
2025-07-17 04:14:05 +0530

    fixed songs are working

--- script.js
+++ script.js
@@ -190,3 +190,179 @@ let bar = document.querySelector('.bars');
 bar.addEventListener('click', () => {
     sidebar.classList.toggle('show_aside');
 })
+
+// ==== Music Player Logic ====
+
+const songs = [
+    {
+      name: "Walking Promises",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+      img: "Images/s1.jpg"
+    },
+    {
+      name: "Desired Games",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      img: "Images/s2.jpg"
+    },
+    {
+      name: "Don't Let Me Down",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+      img: "Images/s3.jpg"
+    },
+    {
+      name: "Dark Alley Acoustic",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
+      img: "Images/s4.jpg"
+    },
+    {
+      name: "Until I Met You",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
+      img: "Images/s5.jpg"
+    },
+    {
+      name: "Desires Pending",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3",
+      img: "Images/s6.jpg"
+    },
+    {
+      name: "Lonely Nights",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3",
+      img: "Images/s1.jpg"
+    },
+    {
+      name: "City Lights",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
+      img: "Images/s2.jpg"
+    },
+    {
+      name: "Rainy Thoughts",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
+      img: "Images/s3.jpg"
+    },
+    {
+      name: "Ocean Drive",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3",
+      img: "Images/s4.jpg"
+    },
+    {
+      name: "Winter Breeze",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-11.mp3",
+      img: "Images/s5.jpg"
+    },
+    {
+      name: "Forever Yours",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-12.mp3",
+      img: "Images/s6.jpg"
+    },
+    {
+      name: "Mountain Echo",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-13.mp3",
+      img: "Images/s1.jpg"
+    },
+    {
+      name: "Sunset Chill",
+      artist: "Harry Jackson, Virginia Hassis",
+      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-14.mp3",
+      img: "Images/s2.jpg"
+    },
+  ];
+  
+  let currentSongIndex = 0;
+  let audio = document.getElementById('main-audio');
+  
+  let playBtn = document.querySelector('.play_button');
+  let nextBtn = document.querySelector('.music_option_next');
+  let prevBtn = document.querySelector('.music_option_prev');
+  
+  let songTitle = document.querySelector('.music_box_text h2');
+  let songArtist = document.querySelector('.music_box_text p');
+  let songImg = document.querySelector('.music_box_img img');
+  
+  function loadSong(index) {
+    const song = songs[index];
+    audio.src = song.src;
+    songTitle.textContent = song.name;
+    songArtist.textContent = song.artist;
+    songImg.src = song.img;
+  }
+  
+  function playSong() {
+    audio.play();
+    playBtn.classList.add('playing');
+  }
+  
+  function pauseSong() {
+    audio.pause();
+    playBtn.classList.remove('playing');
+  }
+  
+  playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+      playSong();
+    } else {
+      pauseSong();
+    }
+  });
+  
+  nextBtn.addEventListener('click', () => {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    loadSong(currentSongIndex);
+    playSong();
+  });
+  
+  prevBtn.addEventListener('click', () => {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    loadSong(currentSongIndex);
+    playSong();
+  });
+  
+  // Autoplay first song on load
+  loadSong(currentSongIndex);
+
+  // Make each song_col clickable to play corresponding song
+  document.addEventListener('DOMContentLoaded', () => {
+    const songCols = document.querySelectorAll('.song_col');
+  
+    console.log("Found song cards:", songCols.length); // Debug log
+  
+    songCols.forEach((col, index) => {
+      col.addEventListener('click', () => {
+        currentSongIndex = index;
+        loadSong(currentSongIndex);
+        playSong();
+      });
+    });
+  });
+
+  let currentTimeEl = document.querySelector('.current_time');
+  let durationEl = document.querySelector('.total_duration');
+  
+  // Format seconds into mm:ss
+  function formatTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = Math.floor(seconds % 60);
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+  }
+  
+  // Update duration when audio metadata is loaded
+  audio.addEventListener('loadedmetadata', () => {
+    durationEl.textContent = formatTime(audio.duration);
+  });
+  
+  // Update current time while playing
+  audio.addEventListener('timeupdate', () => {
+    currentTimeEl.textContent = formatTime(audio.currentTime);
+  });
